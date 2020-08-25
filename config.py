8ffaf706b9d9b24efc54000d6c1b976ed416907f
Raymond Limpens <raymond.limpens@mediaan.com>
2020-08-25 09:50:29 +0200

    Updated configuration file to contain all new configuration variables

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ ON_BOARDING_URL = "https://device.connectivity-suite.cloud/device-api/onboarding
     Iot Core Configuration
 """
 # Connection timeout in seconds
-CONN_DISCONNECTION_TIMEOUT = 10
+CONN_DISCONNECTION_TIMEOUT = 5
 # MQTT Operation timeout in seconds
 MQTT_OPERATION_TIMEOUT = 5
+#Topic Name
+TOPIC_NAME = "hello-world"
 
 """"
     File location configuration
@@ -33,3 +35,8 @@ UDP_ENDPOINT_PORT = 4445
 """
 GPS_TRANSLATION_DATA_PATH = '/flash/lib/translator/gps_data.txt'
 ENV_DATA_COLLECTOR_TRANSLATION_DATA_PATH = '/flash/lib/translator/env_data_coll_data.txt'
+
+""""
+    Message Interval
+"""
+MESSAGE_INTERVAL_SECONDS = 60
